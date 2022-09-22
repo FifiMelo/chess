1747818f075b38a9a94de3aef3437e56ba93e5ef
Felipe <felipe336336@gmail.com>
2022-09-22 12:36:54 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import random
+import time
 
-import math
+
+
+def main():
+    malisz = 0
+    muniak = 0
+    for x in range(10000):
+        if bool(random.getrandbits(1)):
+            malisz += 1
+        else:
+            muniak += 1
+        time.sleep(0.004)
+        print(f"malisz: {malisz} muniak: {muniak}")
+        
+
+
+if __name__ == "__main__":
+    main()
